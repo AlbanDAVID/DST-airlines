44634d2d843d81c8a1eb1d778fcbd30aeb7458d4
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-08-25 15:44:40 +0200

    Add files via upload

--- src/dst_dashboard_docker/setup.sh
+++ src/dst_dashboard_docker/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+$cmd cp static.py docker_images/aws_mysql
+$cmd cp static.py docker_images/dst_dashboard
+
 $cmd docker-compose build --no-cache
-
-
 $cmd docker-compose run aws_mysql_testv1
 $cmd docker-compose run dash_app_testv1
+
+$cmd rm docker_images/aws_mysql/static.py
+$cmd rm docker_images/dst_dashboard/static.py
