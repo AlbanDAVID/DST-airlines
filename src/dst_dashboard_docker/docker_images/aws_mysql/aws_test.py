e3a7d101e20ec38811f1c960a7fc833d88d1bb0b
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-08-25 10:58:30 +0200

    Add files via upload

---
+++ src/dst_dashboard_docker/docker_images/aws_mysql/aws_test.py
@@ -0,0 +1,102 @@
+import pymysql
+import sys
+from pymysql import OperationalError
+import pandas as pd
+import numpy as np
+import os
+from datetime import datetime
+
+
+# date and time during request
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+# Connection test (password and username verification)
+
+try:
+    cnx = pymysql.connect(host = "airlines.cwpriwycnk6a.eu-west-2.rds.amazonaws.com", user = "admin", password ="hyadeb22!")
+    connection = "Successful connection"
+except OperationalError:
+    connection = "Failed connection. Try another username/password"
+
+
+
+output = '''
+==========================================================
+    Connection test (password and username verification)
+    {date_time}
+==========================================================
+
+| username="admin"
+| password="XXXXX"
+
+expected result = Successful connection
+actual restult = {connection}
+
+'''
+
+print(output.format(date_time=dt_string, connection=connection))
+output = output.format(date_time=dt_string, connection=connection)
+
+# print output in a file
+
+with open('/home/log/dash_docker_log.log', 'a') as file:
+    file.write(output)
+
+
+# fetch data 
+
+try:
+    # pour faire des requêtes sql
+
+    # connexion à la BDD
+
+    name_database = 'airlines5'
+    cnx = pymysql.connect(host = "airlines.cwpriwycnk6a.eu-west-2.rds.amazonaws.com", user = "admin", password ="hyadeb22!")
+    cursor = cnx.cursor()
+    use_db = "use {name}".format(name = name_database)
+    cursor.execute(use_db)
+
+    #### Execute query ####
+
+    query1 = ("(SELECT * from FLIGHTS_SUMMARY LEFT JOIN AIRCRAFT ON FLIGHTS_SUMMARY.hex = AIRCRAFT.hex LEFT JOIN FLIGHTS_METRICS ON AIRCRAFT.hex = FLIGHTS_METRICS.hex LEFT JOIN AIRPORTS a ON FLIGHTS_SUMMARY.dep_iata = a.iata_code  LEFT JOIN AIRPORTS b ON FLIGHTS_SUMMARY.arr_iata = b.iata_code LEFT JOIN COUNTRIES as c ON a.country_code = c.code LEFT JOIN COUNTRIES as d ON b.country_code = d.code)")
+    cursor.execute(query1)
+
+
+
+
+    #### Create dataframe from resultant table ####
+    frame = pd.DataFrame(cursor.fetchall())
+
+    frame
+
+    connection = 'successful data fetching'
+
+    
+except NameError:
+
+    connection = "Impossible to fetch data"
+
+
+output = '''
+==========================================================
+    fetch data
+    {date_time}
+==========================================================
+
+
+expected result = successful data fetching
+actual restult = {connection}
+
+'''
+
+print(output.format(date_time=dt_string, connection=connection))
+output = output.format(date_time=dt_string, connection=connection)
+
+# print output in a file
+
+with open('/home/log/dash_docker_log.log', 'a') as file:
+    file.write(output)
+
+
+
