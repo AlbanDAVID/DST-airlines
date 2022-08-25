cec84fbe08fbb6b927e3e7cbc3937ce31ae036ce
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-08-25 10:56:26 +0200

    Add files via upload

---
+++ src/dst_dashboard_docker/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+$cmd docker-compose build --no-cache
+$cmd docker-compose up
---
+++ src/dst_dashboard_docker/static.py
@@ -0,0 +1,8 @@
+# log to connect to aws mysql
+user = 'admin'
+password = 'hyadeb22!'
+host = "airlines.cwpriwycnk6a.eu-west-2.rds.amazonaws.com"
+
+# bdd would like to be used
+name_bdd = 'airlines5'
+
