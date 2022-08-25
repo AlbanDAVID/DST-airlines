4e5cbcab00c22c5733d05178122139be65b40220
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-08-25 15:51:31 +0200

    Update setup.sh

--- src/dst_dashboard_docker/setup.sh
+++ src/dst_dashboard_docker/setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# copy static.py in appropriate folders (before build images)
 $cmd cp static.py docker_images/aws_mysql
 $cmd cp static.py docker_images/dst_dashboard
 
+# build and run images
 $cmd docker-compose build --no-cache
 $cmd docker-compose run aws_mysql_testv1
 $cmd docker-compose run dash_app_testv1
 
+# remove static.py copied before (in the case where we would like to build news images and change static.py)
 $cmd rm docker_images/aws_mysql/static.py
 $cmd rm docker_images/dst_dashboard/static.py
