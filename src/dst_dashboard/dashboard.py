d469e37546586676653b4c7fcbca1fc3a22457b6
HoudaEl-mir <houdaaelmir@gmail.com>
2022-09-02 17:07:45 +0200

    Finished dashboard

--- src/dst_dashboard/dashboard.py
+++ src/dst_dashboard/dashboard.py
@@ -7,8 +7,4 @@ dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.mi
 FA = "https://use.fontawesome.com/releases/v5.12.1/css/all.css"
 app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE, dbc_css, FA])
 
-header = html.H1(
-    "DST Airlines ", className="bg-primary text-white p-2 mb-2 text-center"
-)
-
 
---
+++ src/dst_source/dst_api_test.py
@@ -0,0 +1,39 @@
+import pytest
+import dst_utils as du
+import DstRealtime as dr
+import LufthansaStatic as ls
+import LufthansaVariable as lv
+import AirlabsStatic as als
+import AirlabsVariable as alv
+import json
+
+## Auth
+auth = du.Authentication(client_key="exzk4xtp9pr3txzssb2zqqd4", client_secret="PfMrRRe6AyyB4kTJWdSx")
+def test_auth():
+    header = auth.get_header()
+    assert header.get('accept') == 'application/json'
+
+## Dst static
+
+def test_luf_static_data():
+    rf = du.RequestFactory(auth.get_header())
+    lsi=ls.LufthansaStatic(rf)
+    assert lsi.get_nearest_airports_luf(40, 40, False)['NearestAirportResource']['Airports']['Airport'][0]['AirportCode'] == 'ERC'
+
+## Dst Variable
+
+def test_luf_variable_data():
+    rf = du.RequestFactory(auth.get_header())
+    lsv=lv.LufthansaVariable(rf)
+    assert lsv.get_flight_route("JFK", "FRA",  "2022-08-29", False)['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['AirportCode'] == 'FRA'
+
+
+## DST Real Time
+def test_dst_real_time():
+    api_key = "12de9152-83be-44ae-a711-958190764930"
+
+    # Instanciation de la classe DstRealTime du module dst_extract
+    drt = dr.DstRealTime(api_key)
+
+    # affichage des donnees de tous les vols en temps reel
+    assert drt.get_flights(write_json=False)['request']['currency'] == 'EUR'
