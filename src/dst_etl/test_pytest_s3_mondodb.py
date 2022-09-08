f24d7ab6ea10d73a88d545c75304dd0c8e2ff5d0
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-09-08 17:36:55 +0200

    Update test_pytest_s3_mondodb.py

--- src/dst_etl/test_pytest_s3_mondodb.py
+++ src/dst_etl/test_pytest_s3_mondodb.py
@@ -1,6 +1,6 @@
 import pytest
-from function_test_s3_mongodb import *
-
+from function_test_s3_mongodb import aws_S3_connection
+from function_test_s3_mongodb import aws_mongodb_connection
 def test_aws_S3_connection():
     assert aws_S3_connection() == "Successful connection"
 
