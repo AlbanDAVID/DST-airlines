6bc854d74b4e186495697665a07039a8aed82910
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-09-08 17:30:00 +0200

    Delete test_pytest.py

--- src/dst_etl/test_pytest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from function_test import *
-
-def test_aws_S3_connection():
-    assert aws_S3_connection() == "Successful connection"
-
-
-def test_aws_mongodb_connection():
-    assert aws_mongodb_connection() == 'Successful connection'
