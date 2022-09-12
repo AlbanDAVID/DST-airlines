b531839f15df2913cc91896d73bcb4e67fcbfb1b
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-09-12 14:35:52 +0200

    add static file with login

--- src/airflow/dags/s3_cnx_check.py
+++ src/airflow/dags/s3_cnx_check.py
@@ -2,12 +2,13 @@ from botocore.client import ClientError
 import boto3
 import certifi
 from pymongo import MongoClient
+import static
 
 
 def aws_S3_connection_test():
     
     try:
-        s3_client = boto3.client('s3', aws_access_key_id='AKIAUBEGQUNAZW3YUCNW',aws_secret_access_key='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')
+        s3_client = boto3.client('s3', aws_access_key_id= static.aws_access_key_id,aws_secret_access_key=static.aws_secret_access_key)
         response = s3_client.list_objects_v2(Bucket='datalake-airlines', Prefix='01-real-time-flights-airlabs/')
         connection = "Successful connection"
     except :
