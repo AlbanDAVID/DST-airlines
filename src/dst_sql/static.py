71390d2ee2586104be3a779b36a8adcd806ebe1f
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-10-23 17:50:55 +0200

    delete credentials (for public repo)

--- src/dst_sql/static.py
+++ src/dst_sql/static.py
@@ -1,8 +1,8 @@
 
 # log to connect to aws mysql
-user = 'admin'
-password = 'hyadeb22!'
-host = "airlines.cwpriwycnk6a.eu-west-2.rds.amazonaws.com"
+user = ''
+password = ''
+host = ""
 
 # bdd would like to be used
-name_bdd = 'airlines5'
+name_bdd = ''
