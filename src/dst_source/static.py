58846e54c3e96ee659c2c0def5f873b55339b1cb
AlbanDAVID <61273428+AlbanDAVID@users.noreply.github.com>
2022-10-23 17:50:32 +0200

    delete credentials (for public repo)

--- src/dst_source/static.py
+++ src/dst_source/static.py
@@ -1,32 +1,32 @@
 # log to connect to aws mysql
-user = 'admin'
-password = 'hyadeb22!'
-host = "airlines.cwpriwycnk6a.eu-west-2.rds.amazonaws.com"
+user = ''
+password = ''
+host = ""
 
 # bdd would like to be used
-name_bdd = 'airlines5'
+name_bdd = ''
 
 # plusieurs clés api airlabs ont été créées (pour gérer le nombre de requpetes limitées :)
 # airlabs api
-api_key_airlabs = '5f332e0a-f285-4358-a569-7ac30fc103ba'
+api_key_airlabs = ''
 
 # airlabs api 2
-api_key_airlabs2 = '5f332e0a-f285-4358-a569-7ac30fc103ba'
+api_key_airlabs2 = ''
 
 # airlabs api 3
-api_key_airlabs3 = '5f332e0a-f285-4358-a569-7ac30fc103ba'
+api_key_airlabs3 = ''
 
 # api lufthansa
-client_id_luf = 'exzk4xtp9pr3txzssb2zqqd4'
-client_secret_luf = 'PfMrRRe6AyyB4kTJWdSx'
+client_id_luf = ''
+client_secret_luf = ''
 
 # s3
-aws_access_key_id = 'AKIAUBEGQUNAZW3YUCNW'
-aws_secret_access_key='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'
+aws_access_key_id = ''
+aws_secret_access_key=''
 
 # cnx mongo db 
-cnx_mongodb = "mongodb+srv://user01:wpvPnKP2gdPW8inB@cluster0.tyo5f.mongodb.net/?retryWrites=true&w=majority"
+cnx_mongodb = ""
 
 
 # cnx mysql_aws
-cnx_mysql_aws = "mysql+pymysql://admin:hyadeb22!@airlines.cwpriwycnk6a.eu-west-2.rds.amazonaws.com/airlines5"
+cnx_mysql_aws = ""
